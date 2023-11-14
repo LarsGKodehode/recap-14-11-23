9369f2582d527da307593a0f82204620ff846ec5
LarsG <lars.gunnar@jobloop.no>
2023-11-14 11:40:09 +0000

    add layout component

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,19 @@
+import style from './style.module.css'
+
+export function Layout() {
+  return (
+    <div className={style.app}>
+      <header>
+        <h1>Navbar</h1>
+      </header>
+
+      <main className={style.content}>
+        <p>Content Here</p>
+      </main>
+
+      <footer>
+        <p>Kodeloftet</p>
+      </footer>
+    </div>
+  )
+}

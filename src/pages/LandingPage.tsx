e8d50405b56b99aadc0c6deeb779d5d772e364c6
LarsG <lars.gunnar@jobloop.no>
2023-11-14 11:47:03 +0000

    add landingpage component

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,9 @@
+import style from './LandingPage.module.css'
+
+export function LandingPage() {
+  return (
+    <section className={style.section}>
+      <h2>Hero Section</h2>
+    </section>
+  )
+}

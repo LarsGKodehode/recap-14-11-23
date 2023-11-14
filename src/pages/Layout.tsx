a87bca8eadeeed74d1172cb17baf40090d071d42
LarsG <lars.gunnar@jobloop.no>
2023-11-14 12:02:04 +0000

    add icons

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import style from './Layout.module.css'
 
+import { FaHome, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { Logo } from '../components/Logo/Logo';
+
 type LayoutProps = {
   children: React.ReactNode
 }
@@ -11,7 +14,21 @@ export function Layout(properties: LayoutProps) {
   return (
     <div className={style.app}>
       <header>
-        <h1>Navbar</h1>
+        <Logo />
+
+        <nav>
+          <ul>
+            <li>
+              <FaHome size="4rem" />
+            </li>
+            <li>
+              <FaGithub size="4rem" />
+            </li>
+            <li>
+              <FaLinkedinIn size="4rem" />
+            </li>
+          </ul>
+        </nav>
       </header>
 
       <main className={style.content}>
